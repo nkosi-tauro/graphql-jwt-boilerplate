92f2cfdf7484b7ec7450ac121b1a362eef8a25a0
nkosi-tauro <nkosilati23@yahoo.com>
2020-05-15 01:29:59 +0200

    added auth components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
 import './App.css';
+
+// Pages
+import AuthPage from './pages/Auth'
+import MainNavigation from './components/Navigation/MainNavigation'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <MainNavigation/>
+    <main className="main-content">
+      <Switch>
+        <Redirect from="/" to="/auth" exact />
+        <Route path="/auth" component={AuthPage} />
+      </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,23 +1,99 @@
-import React from 'react';
-import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
-import './App.css';
+import React, {Component} from 'react';
+import './Auth.css'
+import { Error } from 'mongoose';
 
-// Pages
-import AuthPage from './pages/Auth'
-import MainNavigation from './components/Navigation/MainNavigation'
+class AuthPage extends Component{
+    state = {
+        isLogin: true
+    }
+    constructor(props){
+        super(props);
+        this.emailEl = React.createRef();
+        this.passwordEl = React.createRef();
+    }
 
-function App() {
-  return (
-    <BrowserRouter>
-    <MainNavigation/>
-    <main className="main-content">
-      <Switch>
-        <Redirect from="/" to="/auth" exact />
-        <Route path="/auth" component={AuthPage} />
-      </Switch>
-      </main>
-    </BrowserRouter>
-  );
+    switchModeHandler = () => {
+        this.setState(prevState =>{
+            return {isLogin: !prevState.isLogin}
+        })
+    }
+
+    submitHandler = (event)=> {
+        event.preventDefault();
+        const email = this.emailEl.current.value;
+        const password = this.passwordEl.current.value;
+
+        if(email.trim().length === 0|| password.trim().length === 0){
+           return; 
+        }
+        //Send request to backend (graphql)
+        //login Request
+        let requestBody = {
+            query:`
+                query{
+                    login(email: "${email}", password: "${password}"){
+                        userId
+                        token
+                        tokenExpiration
+                    }
+                }
+            `
+        }
+        // SignUp Request
+        if (!this.state.isLogin){
+            requestBody = {
+                query: `
+                    mutation {
+                        createUser(userInput :{email : "${email}", password: "${password}"}){
+                            _id
+                            email
+                        }
+                    }
+                `
+            }
+        }
+ 
+        
+
+        fetch("http://localhost:8000/graphql", {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then(res => {
+            if (res.status != 200 && res.status != 2001 ){
+                throw new Error('Failed');
+            }
+            return res.json();
+        }).then(resData => {
+            console.log(resData)
+        }).catch(err =>{
+            console.log(err)
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <form className="auth-form" onSubmit={this.submitHandler} >
+                    <div className="form-control">
+                        <label htmlFor="email">E-Mail</label>
+                        <input type="email" id="email" ref={this.emailEl}/>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" ref={this.passwordEl}/>
+                    </div>
+                    <div className="form-actions">
+                        <button type="submit">Submit</button>
+                        <button type="button"onClick={this.switchModeHandler}> Switch to {this.state.isLogin ? 'Signup' : 'Login'}</button>
+                        
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default AuthPage;
